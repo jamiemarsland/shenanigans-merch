ebc0453cb8ce892417b076244e74ebd64d6b0aad
jamiemarsland <jamiemarsland@pootlepress.co.uk>
2026-09-08 22:34:58 +0100

    Fix product heading and column sizing; add branded theme thumbnail

--- shenanigans-merch/functions.php
+++ shenanigans-merch/functions.php
@@ -9,8 +9,8 @@ add_action( 'after_setup_theme', function () {
     add_editor_style( 'assets/shop.css' );
 } );
 add_action( 'wp_enqueue_scripts', function () {
-    wp_enqueue_style( 'shenanigans-shop', get_theme_file_uri( 'assets/shop.css' ), array(), '1.0.0' );
+    wp_enqueue_style( 'shenanigans-shop', get_theme_file_uri( 'assets/shop.css' ), array(), '1.0.1' );
     if ( function_exists( 'is_product' ) && is_product() ) {
-        wp_enqueue_script( 'shenanigans-sizes', get_theme_file_uri( 'assets/sizes.js' ), array( 'jquery', 'wc-add-to-cart-variation' ), '1.0.0', true );
+        wp_enqueue_script( 'shenanigans-sizes', get_theme_file_uri( 'assets/sizes.js' ), array( 'jquery', 'wc-add-to-cart-variation' ), '1.0.1', true );
     }
 } );
